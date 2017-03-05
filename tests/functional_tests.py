75b5368935308ef6d92c4abea6fae6590a14b409
Hugo Alvarado <hugo102@gmail.com>
2017-03-05 01:07:22 -0600

    Add initial functional test class

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
+import unittest
 
-#used Chrome webdriver from http://chromedriver.storage.googleapis.com/index.html?path=2.27/
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        # used Chrome webdriver from http://chromedriver.storage.googleapis.com/index.html?path=2.27/
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_search_for_yamaha_and_get_results(self):
+        self.browser.get('http://localhost:8000')
+
+        assert 'FindMyWheels' in self.browser.title
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
